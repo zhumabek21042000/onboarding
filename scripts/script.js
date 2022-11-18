9209352afec1c688832ca56a303fc0fa2dbfff59
zhumabek21042000 <zhumabek.gubash@gmail.com>
2022-11-18 06:22:07 +0500

    fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,70 @@
-function next(id){
-    var currentPage = document.getElementById(`page-${id}`);
-    var nextPage = document.getElementById(`page-${parseInt(id)+1}`);
-    currentPage.classList.add("hidden");
-    nextPage.classList.remove("hidden");
+function next(id) {
+  var currentPage = document.getElementById(`page-${id}`);
+  var nextPage = document.getElementById(`page-${parseInt(id) + 1}`);
+  currentPage.classList.add("hidden");
+  nextPage.classList.remove("hidden");
 
-    if(parseInt(id) === 1){
-        couponsAnimation();
-    }
+  var prize = document.getElementById("prize");
+
+  if (parseInt(id) === 2) {
+    setTimeout(() => {
+      prize.classList.remove("hidden");
+      prize.previousElementSibling.classList.add("hidden");
+    }, 4500);
+  } else {
+    setTimeout(() => {
+      prize.classList.add("hidden");
+      prize.previousElementSibling.classList.remove("hidden");
+    }, 100);
+  }
 }
 
-function back(id){
-    var currentPage = document.getElementById(`page-${id}`);
-    var previousPage = document.getElementById(`page-${parseInt(id) - 1}`);
-    currentPage.classList.add("hidden");
-    previousPage.classList.remove("hidden")
-    if(parseInt(id) - 1 === 2){
-        couponsAnimation();
-    }
+function back(id) {
+  var currentPage = document.getElementById(`page-${id}`);
+  var previousPage = document.getElementById(`page-${parseInt(id) - 1}`);
+  currentPage.classList.add("hidden");
+  previousPage.classList.remove("hidden");
+
+  var prize = document.getElementById("prize");
+  if (parseInt(id) - 1 === 3) {
+    setTimeout(() => {
+      prize.classList.remove("hidden");
+      prize.previousElementSibling.classList.add("hidden");
+    }, 4500);
+  } else {
+    setTimeout(() => {
+      prize.classList.add("hidden");
+      prize.previousElementSibling.classList.remove("hidden");
+    }, 100);
+  }
 }
 
-function couponsAnimation(){
-    $(function(){
-        $("#coupon_list").load("../coupon.html"); 
-      });
+function couponsAnimation() {
+  $(function () {
+    $("#coupon_list").load("../coupon.html");
+  });
 }
 
-function cardsAnimation(){
-    
+function openLogin() {
+  console.log("login");
+  if (window?.Android && typeof window.Android.openLogin == "function") {
+    Android.openLogin();
+    return null;
+  } else if (window.webkit && window.webkit.messageHandlers) {
+    webkit.messageHandlers.performAction.postMessage({
+      action: "openLogin",
+    });
+  }
+}
+
+function openRegistration() {
+  console.log("registration");
+  if (window?.Android && typeof window.Android.openRegistration == "function") {
+    Android.openRegistration();
+    return null;
+  } else if (window.webkit && window.webkit.messageHandlers) {
+    webkit.messageHandlers.performAction.postMessage({
+      action: "openRegistration",
+    });
+  }
 }
