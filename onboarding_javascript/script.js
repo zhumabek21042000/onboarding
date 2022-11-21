0f8650d39821517de5fd47dd0e52d56d39e38e8b
zhumabek21042000 <zhumabek.gubash@gmail.com>
2022-11-21 21:14:24 +0600

    finish

--- onboarding_javascript/script.js
+++ onboarding_javascript/script.js
@@ -1,4 +1,22 @@
 function next(id) {
+  var backButton = document.getElementById("back-button");
+  var skipButton = document.getElementById("skip-button");
+  var nextButton = document.getElementById("next-button");
+  var bottomBar = document.getElementById("bottom");
+  var dots = document.getElementById("dots");
+  if (parseInt(id) === 1) {
+    backButton.classList.remove("invisible");
+    skipButton.classList.remove("skip");
+    skipButton.classList.add("red");
+  } else if (parseInt(id) === 5) {
+    skipButton.classList.add("invisible");
+    nextButton.classList.add("invisible");
+    bottomBar.classList.add("invisible");
+  }
+  nextButton.setAttribute("onclick", `next(${parseInt(id) + 1})`);
+  skipButton.setAttribute("onclick", `skip(${parseInt(id) + 1})`);
+  backButton.setAttribute("onclick", `back(${parseInt(id) + 1})`);
+
   var currentPage = document.getElementById(`page-${id}`);
   var nextPage = document.getElementById(`page-${parseInt(id) + 1}`);
   currentPage.classList.add("hidden");
@@ -17,9 +35,31 @@ function next(id) {
       prize.previousElementSibling.classList.remove("hidden");
     }, 100);
   }
+  dots.children[parseInt(id) - 1].classList.remove("active");
+  dots.children[parseInt(id)].classList.add("active");
 }
 
 function back(id) {
+  var backButton = document.getElementById("back-button");
+  var skipButton = document.getElementById("skip-button");
+  var nextButton = document.getElementById("next-button");
+  var bottomBar = document.getElementById("bottom");
+  var dots = document.getElementById("dots");
+
+  if (parseInt(id) === 2) {
+    backButton.classList.add("invisible");
+    skipButton.classList.add("skip");
+    skipButton.classList.remove("red");
+    backButton.removeAttribute("onclick");
+    skipButton.setAttribute("onclick", `skip(${parseInt(id) - 1})`);
+  } else if (parseInt(id) === 6) {
+    nextButton.classList.remove("invisible");
+    bottomBar.classList.remove("invisible");
+    skipButton.classList.remove("invisible");
+  }
+  skipButton.setAttribute("onclick", `skip(${parseInt(id) - 1})`);
+  backButton.setAttribute("onclick", `back(${parseInt(id) - 1})`);
+  nextButton.setAttribute("onclick", `next(${parseInt(id) - 1})`);
   var currentPage = document.getElementById(`page-${id}`);
   var previousPage = document.getElementById(`page-${parseInt(id) - 1}`);
   currentPage.classList.add("hidden");
@@ -37,20 +77,32 @@ function back(id) {
       prize.previousElementSibling.classList.remove("hidden");
     }, 100);
   }
+  if (parseInt(id) === 6) {
+    dots.children[4].classList.add("active");
+  } else {
+    dots.children[parseInt(id) - 1].classList.remove("active");
+    dots.children[parseInt(id) - 2].classList.add("active");
+  }
 }
 
-function skip(id){
+function skip(id) {
+  var backButton = document.getElementById("back-button");
+  var skipButton = document.getElementById("skip-button");
+  var bottomBar = document.getElementById("bottom");
   var currentPage = document.getElementById(`page-${id}`);
-  currentPage.classList.add("hidden");
-
   var lastPage = document.getElementById("page-6");
-  lastPage.classList.remove("hidden")
-}
+  var dots = document.getElementById("dots");
+  dots.children[parseInt(id) - 1].classList.remove("active");
+  currentPage.classList.add("hidden");
+  lastPage.classList.remove("hidden");
+  if (parseInt(id) === 1) {
+    backButton.classList.remove("invisible");
+    skipButton.classList.remove("skip");
+  }
 
-function couponsAnimation() {
-  $(function () {
-    $("#coupon_list").load("../coupon.html");
-  });
+  backButton.setAttribute("onclick", `back(6)`);
+  skipButton.classList.add("invisible");
+  bottomBar.classList.add("invisible");
 }
 
 function openLogin() {
