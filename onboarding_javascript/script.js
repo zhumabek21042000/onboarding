8d64d5fa3d03246429c09204c4c511b7a4480a2d
zhumabek21042000 <zhumabek.gubash@gmail.com>
2022-11-24 03:09:53 +0600

    fixes

--- onboarding_javascript/script.js
+++ onboarding_javascript/script.js
@@ -1,30 +1,69 @@
 function next(id) {
   var currentPage = document.getElementById(`page-${id}`);
   var nextPage = document.getElementById(`page-${parseInt(id) + 1}`);
+  var currentDots = document.getElementById(`dots-${parseInt(id)}`);
+  var nextDots = document.getElementById(`dots-${parseInt(id) + 1}`);
+
+  var couponList = document.getElementById("coupon_list");
+
+  if (nextDots && currentDots) {
+    setTimeout(() => {
+      nextDots.children[parseInt(id)].classList.add("active");
+      currentDots.children[parseInt(id) - 1].classList.remove("active");
+      if (parseInt(id) + 1 === 2) {
+        couponList.classList.add("coupon_list");
+      } else {
+        couponList.classList.remove("coupon_list");
+      }
+    }, 50);
+  }
+  if (parseInt(id) + 1 === 2) {
+    couponList.classList.add("coupon_list");
+  } else {
+    couponList.classList.remove("coupon_list");
+  }
+
   currentPage.classList.add("hidden");
   nextPage.classList.remove("hidden");
-
   var prize = document.getElementById("prize");
-
   if (parseInt(id) === 2) {
     setTimeout(() => {
       prize.classList.remove("hidden");
       prize.previousElementSibling.classList.add("hidden");
     }, 2500);
   } else {
-    setTimeout(() => {
-      prize.classList.add("hidden");
-      prize.previousElementSibling.classList.remove("hidden");
-    }, 100);
+    prize.classList.add("hidden");
+    prize.previousElementSibling.classList.remove("hidden");
   }
 }
 
 function back(id) {
   var currentPage = document.getElementById(`page-${id}`);
   var previousPage = document.getElementById(`page-${parseInt(id) - 1}`);
+  var currentDots = document.getElementById(`dots-${parseInt(id)}`);
+  var nextDots = document.getElementById(`dots-${parseInt(id) - 1}`);
+  var couponList = document.getElementById("coupon_list");
+
+  if (nextDots && currentDots) {
+    setTimeout(() => {
+      nextDots.children[parseInt(id) - 2].classList.add("active");
+      currentDots.children[parseInt(id) - 1].classList.remove("active");
+      if (parseInt(id) - 1 === 2) {
+        couponList.classList.add("coupon_list");
+      } else {
+        couponList.classList.remove("coupon_list");
+      }
+    }, 50);
+  }
+
+  if (parseInt(id) - 1 === 2) {
+    couponList.classList.add("coupon_list");
+  } else {
+    couponList.classList.remove("coupon_list");
+  }
+
   currentPage.classList.add("hidden");
   previousPage.classList.remove("hidden");
-
   var prize = document.getElementById("prize");
   if (parseInt(id) - 1 === 3) {
     setTimeout(() => {
@@ -32,19 +71,17 @@ function back(id) {
       prize.previousElementSibling.classList.add("hidden");
     }, 2500);
   } else {
-    setTimeout(() => {
-      prize.classList.add("hidden");
-      prize.previousElementSibling.classList.remove("hidden");
-    }, 100);
+    prize.classList.add("hidden");
+    prize.previousElementSibling.classList.remove("hidden");
   }
 }
 
-function skip(id){
+function skip(id) {
   var currentPage = document.getElementById(`page-${id}`);
   currentPage.classList.add("hidden");
 
   var lastPage = document.getElementById("page-6");
-  lastPage.classList.remove("hidden")
+  lastPage.classList.remove("hidden");
 }
 
 function couponsAnimation() {
@@ -63,7 +100,6 @@ function openLogin() {
     });
   }
 }
-
 function openRegistration() {
   if (window?.Android && typeof window.Android.openRegistration == "function") {
     Android.openRegistration();
@@ -71,6 +107,20 @@ function openRegistration() {
   } else if (window.webkit && window.webkit.messageHandlers) {
     webkit.messageHandlers.performAction.postMessage({
       action: "openRegistration",
+    });
+  }
+}
+
+function openLink(url) {
+  if (window?.Android && typeof window.Android.openInBrowser == "function") {
+    Android.openInBrowser(url);
+    return;
+  } else if (window.webkit && window.webkit.messageHandlers) {
+    webkit.messageHandlers.performAction.postMessage({
+      action: "openInBrowser",
+      data: {
+        url: url,
+      },
     });
   }
 }
